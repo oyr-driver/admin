16f614b707f34167c921c643a3089c5f5a2325b3
dahyunko <kdhgby@naver.com>
2022-08-05 21:53:19 +0900

    first admin log

--- app.js
+++ app.js
@@ -10,14 +10,55 @@ const port = 5000;
 app.set('views','./views');
 app.set('view engine', 'ejs');
 
-app.use(express.static('/assets'));
+app.use(express.static("assets"));
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render('charts-chartjs');
-//   res.render('index');
+app.get('/login', (req, res) => {
+  res.render('login');
 })
 
+app.get('/layout', (req, res) => {
+  // res.render('charts-chartjs');
+  res.render('forms-layouts');
+})
+
+app.post('/call', (req, res) => {
+  res.render('call');
+})
+
+app.post('/auth', (req, res) => {
+  res.render('auth');
+})
+
+app.post('/user', (req, res) => {
+  res.render('user');
+})
+
+app.get('/call', (req, res) => {
+  res.render('call');
+})
+
+app.get('/auth', (req, res) => {
+  res.render('auth');
+})
+
+app.get('/user', (req, res) => {
+  res.render('user');
+})
+
+//회원가입 페이지로 이동
+app.get('/create',function(req,res){
+  res.sendFile(__dirname + "/views/create_user/add.html");
+})
+
+app.get('/logout',(req,res)=>{
+    console.log('로그아웃 성공');
+    // req.session.destroy(function(err){
+    //     res.redirect('/');
+    // });
+    res.redirect('/login');
+
+})
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
