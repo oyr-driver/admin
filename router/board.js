bff598751b4dfd38ca34f06c44a20fd28707e2b1
dahyunko <kdhgby@naver.com>
2022-08-10 16:32:17 +0900

    join membership

--- router/board.js
+++ router/board.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const router =  express.Router();
 
+//mysql연동
+const mysql = require('mysql');
+const dbconfig = require('../config/database.js');//db router
+const connection = mysql.createConnection(dbconfig);
+
 //body-parser이용
 // router.post('/call',function(req,res){
 //   var user = req.body.user;
@@ -26,6 +31,16 @@ router.post('/company/cons', (req, res) => {
     res.render('consultant');
 })
 
+//상담원 삭제 기능 구현
+router.get('/company/cons/delete/:id',(req,res)=>{
+    const sql = "DELETE FROM g_consultant WHERE conID = ?";
+    connection.query(sql,[req.params.conID],(err, result, field)=>{
+        if(err) throw err;
+        console.log(result);
+        res.redirect('/company/cons');
+    })
+})
+
 router.post('/company/user', (req, res) => {
     res.render('user');
 })
@@ -40,11 +55,23 @@ router.get('/auth', (req, res) => {
 })
 
 router.get('/company/com', (req, res) => {
-    res.render('company');
+    const sql = "SELECT * FROM g_company";
+    connection.query(sql,(err, result,field)=>{
+        if(err) throw err;
+        console.log(result);
+        res.render('company',{company:result});        
+    });
+
+    // res.render('company');
 })
 
 router.get('/company/user', (req, res) => {
-    res.render('user');
+    const sql = "SELECT * FROM g_user";
+    connection.query(sql,(err, result,field)=>{
+        if(err) throw err;
+        console.log(result);
+        res.render('user',{user:result});        
+    });
 })
 
 router.get('/company/cons', (req, res) => {
--- router/login_out.js
+++ router/login_out.js
@@ -13,17 +13,17 @@ router.get('/login', (req, res) => {
 
 //로그인
 router.post('/call', (req, res) => {
-    const user = req.body.user;
+    const conID = req.body.conID;
     const pw = req.body.password;
     const cpID = req.body.cpID;
 
-    if(user && pw && cpID){
-        connection.query('SELECT * FROM user WHERE cpID=? and cpPW=? and cpNM=?',[user,pw,cpID],function(error, result){
+    if(conID && pw && cpID){
+        connection.query('SELECT * FROM g_consultant WHERE conID=? and conPW=? and cpID=?',[conID,pw,cpID],function(error, result){
             if(error) throw error;
             if(result.length>0){//로그인 성공
                 //session 정보 저장
                 req.session.users = {
-                    user_ID: user,
+                    user_ID: conID,
                     user_PW:pw,
                     user_CP: cpID 
                 };
@@ -47,15 +47,29 @@ router.post('/call', (req, res) => {
 
   //회원가입 페이지로 이동
 router.get('/create',function(req,res){
-    res.sendFile(__dirname + "/views/create_user/add.html");
+    res.render('add');
+})
+
+router.post('/create',(req,res)=>{
+    const sql = "INSERT INTO g_consultant SET ?"
+
+    connection.query(sql,req.body,(err,result,field)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(`
+            <script>
+                alert("회원가입 성공");
+                location.href="/login";
+            </script>
+        `);
+    })
 })
 
 router.get('/logout',(req,res)=>{
     console.log('로그아웃 성공');
-      // req.session.destroy(function(err){
-      //     res.redirect('/');
-      // });
-    res.redirect('/login');
+        req.session.destroy(function(err){
+        res.redirect('/login');
+    });
 });
 
 module.exports = router;
