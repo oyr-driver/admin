05637f86ac3a159c831882b893a8e5d6f163fc56
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2022-09-03 20:51:58 +0900

    add cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const port = 3010;
 // 클라이언트 접근 허용
 const cors = require('cors')
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "https://u.goodde.kr" }));
 //{ credentials: true, origin: "http://www.goodde.kr:3000" }
 
 //view engine setup
